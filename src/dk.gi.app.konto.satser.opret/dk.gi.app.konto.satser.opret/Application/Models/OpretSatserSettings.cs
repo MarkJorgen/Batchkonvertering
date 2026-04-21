7352ab9e1e979ec093770755b60eef7ff917dc6d
MarkJorgen <43058180+MarkJorgen@users.noreply.github.com>
2026-04-21 16:33:15 +0200

    Tilføj settingsmodel for satser.opret

---
+++ src/dk.gi.app.konto.satser.opret/dk.gi.app.konto.satser.opret/Application/Models/OpretSatserSettings.cs
@@ -0,0 +1,23 @@
+namespace dk.gi.app.konto.satser.opret.Application.Models
+{
+    public sealed class OpretSatserSettings
+    {
+        public JobExecutionMode Mode { get; set; }
+        public string AuthorityMode { get; set; }
+        public string RuntimeEngine { get; set; }
+        public bool EnableLegacySettingWriteOut { get; set; }
+        public int SatsAar { get; set; }
+        public int TimeOutMinutter { get; set; }
+        public int SecondsToSleep { get; set; }
+        public int MaxWaitCount { get; set; }
+        public string CrmConnectionTemplate { get; set; }
+        public string CrmServerName { get; set; }
+        public string CrmClientId { get; set; }
+        public string CrmClientSecret { get; set; }
+        public string CrmAuthority { get; set; }
+        public string CrmOrganisationName { get; set; }
+        public string CrmUserName { get; set; }
+        public string CrmUserPassword { get; set; }
+        public string FailureRecipients { get; set; }
+    }
+}
